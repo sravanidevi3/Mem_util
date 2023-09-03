711bb41d5c8441cb13fd68831f8ae88bf8af686c
sravanidevi3 <77570091+sravanidevi3@users.noreply.github.com>
2023-09-03 15:03:39 +0530

    Update memory_usage.py

--- memory_usage.py
+++ memory_usage.py
@@ -33,7 +33,7 @@ with open(csv_path , 'r' , encoding="utf-8-sig")as csv_file:
         script = "free -m"
 
         try:
-            out_results=mem_util([ip,username,password,script])
+            out_results=mem_util([ip , username , password , script])
             mem_util=out_results[1].strip()
             results.append([ip , mem_util])
 
