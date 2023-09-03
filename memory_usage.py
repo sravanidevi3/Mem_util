38fc5719a0b88b0b18026e6e7a23237428f3e695
sravanidevi3 <77570091+sravanidevi3@users.noreply.github.com>
2023-09-03 16:27:34 +0530

    Update memory_usage.py

--- memory_usage.py
+++ memory_usage.py
@@ -1,8 +1,8 @@
-import sys
-import os
 import csv
-import paramiko
+import os
 import traceback
+import sys
+import paramiko
 
 username = sys.argv[1]
 password = sys.argv[2]
@@ -12,39 +12,42 @@ reportpath = sys.argv[4]
 csv_path = os.path.join(csvpath,"servers.csv")
 report_path = os.path.join(reportpath,"Healthcheck.csv")
 
-def mem_utilty(ip: str , username: str , password: str , script: str):
-    print("Attempting ssh connection")
-    #Establish ssh connection
-    client=paramiko.SSHClient()
+def mem_utility(ip:str , username:str, password:str, script:str):
+    print(f"Attempting ssh connection")
+    #Establishing ssh connection
+    client = paramiko.SSHClient()
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     client.connect(hostname=ip , username=username , password=password)
-
+    
     stdin,stdout,stderr=client.exec_command(script)
-    return stdout.readlines()
+    return stderr.readlines()
 
-with open(csv_path , 'r' , encoding="utf-8-sig")as csv_file:
+with open(csv_path , 'r' , encoding="utf-8-sig") as csv_file:
     csv_reader = csv.reader(csv_file , delimiter=',')
 
     results=[]
     for row in csv_reader:
         ip = row[0]
-        port = 22
         hostname = ip
+        port = 22
+
         script = "free -m"
 
+
         try:
-            out_results = mem_utilty(ip , username , password , script)
+            out_results = mem_utility(ip , username , password , script)
             mem_util = out_results[1].strip()
-            results.append([ip , mem_util])
+            results.append([ip,mem_util])
 
         except Exception as e:
-            print(f"An error occured for {ip}: {e}")
+            print(f"An error has occured as {e}") 
 
-with open(report_path , 'w', newline = '' ,encoding="utf-8-sig")as report_file:
-    csv_writer=csv.writer(report_file)
+
+with open(report_path , 'w' , newline = '' ,encoding = 'utf-8-sig') as report_file:
+    csv_writer = csv.writer(report_file)
     csv_writer.writerows(results)
-    csv_writer.writerow(["IP","Mem_util"])
+    csv_writer.writerow(["IP" , "MEM_UTILITY"])
 
-print("Healthcheck is saved in Healthcheck.csv")
+print("Healthcheck is saved in healthcheck.csv")
 
 
