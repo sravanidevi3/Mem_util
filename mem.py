58020e641641035800a157180353e210a451fbae
sravanidevi3 <77570091+sravanidevi3@users.noreply.github.com>
2023-09-03 13:37:00 +0530

    Update mem.py

--- mem.py
+++ mem.py
@@ -1,55 +1,49 @@
-import sys
 import os
+import csv
 import paramiko
 import traceback
-import csv
+import sys
 
 username = sys.argv[1]
 password = sys.argv[2]
 csvpath = sys.argv[3]
 reportpath = sys.argv[4]
 
+csv_fullpath = os.path.join(csvpath , "server.csv")
+report_fullpath = os.path.join(reportpath , "reportfull.csv")
 
-csv_path = os.path.join(csvpath,"servers.csv")
-report_path = os.path.join(reportpath,"Healthcheck.csv")
-
-
-with open(csv_path , 'r' , encoding='utf-8-sig') as csv_file:
-    csv_reader = csv.reader(csv_file , delimiter=',')
-
-#store the results in list format
-results = []
-
-for row in csv_reader:
-    ip = row[0]
-    port = 22
-    hostname = ip
-
-    script = "free -m"
-
-    try:
-        out_results = mem_util([ip , username , password , script])
-        mem_util = out_results[1].strip()
-        results.append([ip,mem_util])
-
-    except Exception as e:
-        print("An error has occured for {ip} as {e}")
-
-
-with open(report_path , 'w' , newline = '' , encoding = 'utf-8-sig') as report_file:
-    csv_writer = csv.writer(report_file)
-    csv_writer.writerow(['IP' , 'Mem_util'])
-    csv_writer.writerow(results)
-
-def mem_util(ip:str , username:str , password:str , script:str):
-    #Establish SSH Connection
-    print("Attemping ssh connection for {ip}")
+def mem_util(ip , username , password , script):
+    #Establishing connection.
     client = paramiko.SSHClient()
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    client.connect([hostname , username , password])
+    client.connect(hostname = ip , username = username , password = password)
 
-    stdin,stdout,stderr=client.exec_command(script)
+    stdin,stdout,stderr = client.exec_command(script)
+
     return stdout.readlines()
 
-print("Healthcheck is saved Healthcheck.csv")
+with open(csv_fullpath , 'r' , encoding= "utf-8-sig") as csv_file:
+    csv_reader = csv.reader(csv_file , delimiter=',')
 
+    results = []
+
+    for row in csv_reader:
+        ip = row[0]
+        port = 22
+        hostname = ip
+
+        script = "df -h"
+        try:
+            out_result = mem_util(ip , username , password , script)
+            mem_util = results[2].strip()
+            results.append(ip , mem_util)
+        except Exception as e:
+            print ("an errro occurred while authenticates as {e}")
+
+with open (report_fullpath , 'w', newline= '' , encoding="utf-8-sig") as reportfile:
+    csv_writer = csv.writer(reportfile)
+    csv_writer.writerow("ip", "utilization")
+    csv_writer.writerows(results)
+
+
+print("health check is created")
